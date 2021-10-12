29559ce30092291bca5b9241c087c6b06b09912b
Baily Chen <bailyachen@gmail.com>
2021-10-12 05:48:27 -0700

    Added Models

--- src/App.js
+++ src/App.js
@@ -1,29 +1,79 @@
 import './App.css';
-import { Canvas, useFrame, useThree, useLoader, extend } from '@react-three/fiber';
-import { useRef, Suspense } from 'react';
+import { Canvas, useFrame, useThree, extend } from '@react-three/fiber';
+import { useRef, useEffect, useState, Suspense } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-extend({ OrbitControls })
+import { DragControls } from 'three/examples/jsm/controls/DragControls';
+extend({ OrbitControls });
+extend({ DragControls });
+
+const Dragable = props => {
+  const groupRef = useRef();
+  const controlsRef = useRef();
+  const [children, setChildren] = useState([]);
+  const {camera, gl, scene} = useThree();
+  useEffect(() => {
+    setChildren(groupRef.current.children)
+  }, []);
+  useEffect(() => {
+    controlsRef.current.addEventListener(
+      'hoveron',
+      e => {
+        scene.orbitControls.enabled = false;
+        if (scene.orbitControls.enabled) console.log("Failed");
+      }
+    );
+    controlsRef.current.addEventListener(
+      'hoveroff',
+      e => scene.orbitControls.enabled = true
+    );
+  }, [children, scene.orbitControls]);
+  
+  
+  return (
+    <group ref={groupRef}>
+      <dragControls ref={controlsRef} args={[children, camera, gl.domElement]}/>
+      {props.children}
+    </group>
+  );
+}
 
 const Orbit = () => {
   const {camera, gl} = useThree();
   return (
-    <orbitControls args={[camera, gl.domElement]}/>
+    <orbitControls attach='orbitControls' args={[camera, gl.domElement]}/>
   );
 }
 
 const Cube = props => {
   const ref = useRef();
-  const texture = useLoader(THREE.TextureLoader, '/wood.jpg');
   useFrame(state => {
     ref.current.rotation.x += 0.01;
     ref.current.rotation.y += 0.01;
   });
   return (
-    <mesh ref={ref} {...props} castShadow>
+    <mesh 
+      ref={ref} 
+      {...props} 
+      castShadow
+      onPointerDown={(e) => {
+        e.object.active = true;
+        window.activeMesh = e.object;
+      }}
+      onPointerEnter={(e) => {
+        ref.current.scale.x += 0.5;
+        ref.current.scale.y += 0.5;
+        ref.current.scale.z += 0.5;
+      }}
+      onPointerLeave={(e) => {
+        ref.current.scale.x -= 0.5;
+        ref.current.scale.y -= 0.5;
+        ref.current.scale.z -= 0.5;
+      }}
+    >
       <boxBufferGeometry/>
       <meshPhysicalMaterial
-      map={texture}
+        color='red'
       />
     </mesh>
   );
@@ -49,20 +99,45 @@ const Bulb = props => {
 }
 
 function App() {
+  const handleClick = e => {
+    if (!window.activeMesh) {
+      return;
+    }
+    window.activeMesh.material.color = new THREE.Color(e.target.style.background);
+  }
   return (
     <div style={{height: '100vh', width: '100vw'}}>
+      <div style={{position: 'absolute', zIndex: 1}}>
+        <div 
+          onClick={handleClick}
+          style={{ background: 'blue', height: 50, width: 50}}
+        />
+        <div 
+          onClick={handleClick}
+          style={{ background: 'red', height: 50, width: 50}}
+        />
+        <div 
+          onClick={handleClick}
+          style={{ background: 'yellow', height: 50, width: 50}}
+        />
+        <div 
+          onClick={handleClick}
+          style={{ background: 'green', height: 50, width: 50}}
+        />
+      </div>
       <Canvas 
-      style={{background: 'black'}}
-      camera={{position: [3, 3, 3]}}
-      shadows
+        style={{background: 'black'}}
+        camera={{position: [3, 3, 3]}}
+        shadows
       >
         <Orbit/>
         <Bulb position={[2, 6, 0]}/>
-        <Suspense fallback={null}>
-          <Cube position={[0, 0, 0]}/>
-        </Suspense>
+        <Dragable>
+          <Suspense fallback={null}>
+            <Cube position={[0, 0, 0]}/>
+          </Suspense>
+        </Dragable>
         <Floor position={[0, -3, 0]}/>
-        {/*<fog attach='fog' args={['white', 1, 10]}/>*/}
       </Canvas>
     </div>
   );
